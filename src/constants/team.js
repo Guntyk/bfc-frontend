f57bc9a52401a316f48e6f7360595204d33259b1
Daniil <mashyn.daniil@gmail.com>
2024-01-04 17:41:18 +0200

    Team correction

---
+++ src/constants/team.js
@@ -0,0 +1,410 @@
+export const team = [
+  {
+    id: 1,
+    attributes: {
+      name: 'Леонід ',
+      surname: 'Хоменко ',
+      fathername: 'Вадимович',
+      role: 'Голова громадської організації',
+      createdAt: '2023-12-31T00:47:19.517Z',
+      updatedAt: '2024-01-04T12:11:41.861Z',
+      publishedAt: '2023-12-31T00:47:20.134Z',
+      description: [
+        {
+          type: 'list',
+          format: 'unordered',
+          children: [
+            {
+              type: 'list-item',
+              children: [
+                {
+                  type: 'text',
+                  text: 'Народився 11 травня 2001 року.',
+                },
+              ],
+            },
+            {
+              type: 'list-item',
+              children: [
+                {
+                  type: 'text',
+                  text: 'Бакалавр юридичних наук та на даний момент здобуває магістра права у Київському Національному Універститеті імені Тараса Шевченка.',
+                },
+              ],
+            },
+            {
+              type: 'list-item',
+              children: [
+                {
+                  type: 'text',
+                  text: 'Голова Фракції «Незалежні».',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          type: 'paragraph',
+          children: [
+            {
+              type: 'text',
+              text: 'Коментар щодо створення фракції від Леоніда Хоменка: ',
+            },
+          ],
+        },
+        {
+          type: 'quote',
+          children: [
+            {
+              type: 'text',
+              text: 'На жаль, ми маємо справу з прогалинами в законодавстві, такими як відсутність заборони на працевлаштування депутатів місцевих рад у апараті тієї ж сільскої ради. Створення фракції - це спершу всього об’єднання депутатів з однаковими поглядами на ту чи іншу ситуацію. Те, що відбувається під час війни, на думку фракції, не припустимо.',
+            },
+          ],
+        },
+        {
+          type: 'paragraph',
+          children: [
+            {
+              type: 'text',
+              text: 'Коментар щодо створення громадської організації:',
+            },
+          ],
+        },
+        {
+          type: 'quote',
+          children: [
+            {
+              type: 'text',
+              text: 'Мета та цілі громадської організації «Світле майбутнє громади» - це домогтися прозорості, підзвітності та діалогу між громадою та Гірською сільською радою. Лише об’єднання та активна громадянська позиція зможе принести цей результат. Формула роботи у нас є!',
+            },
+          ],
+        },
+      ],
+      photo: {
+        data: {
+          id: 8,
+          attributes: {
+            name: 'IMG_7168.JPG',
+            alternativeText: null,
+            caption: null,
+            width: 2048,
+            height: 2047,
+            formats: {
+              thumbnail: {
+                name: 'thumbnail_IMG_7168.JPG',
+                hash: 'thumbnail_IMG_7168_48fd09f204',
+                ext: '.JPG',
+                mime: 'image/jpeg',
+                path: null,
+                width: 157,
+                height: 156,
+                size: 8.66,
+                url: '/uploads/thumbnail_IMG_7168_48fd09f204.JPG',
+              },
+              large: {
+                name: 'large_IMG_7168.JPG',
+                hash: 'large_IMG_7168_48fd09f204',
+                ext: '.JPG',
+                mime: 'image/jpeg',
+                path: null,
+                width: 1000,
+                height: 1000,
+                size: 164.19,
+                url: '/uploads/large_IMG_7168_48fd09f204.JPG',
+              },
+              small: {
+                name: 'small_IMG_7168.JPG',
+                hash: 'small_IMG_7168_48fd09f204',
+                ext: '.JPG',
+                mime: 'image/jpeg',
+                path: null,
+                width: 500,
+                height: 500,
+                size: 50.21,
+                url: '/uploads/small_IMG_7168_48fd09f204.JPG',
+              },
+              medium: {
+                name: 'medium_IMG_7168.JPG',
+                hash: 'medium_IMG_7168_48fd09f204',
+                ext: '.JPG',
+                mime: 'image/jpeg',
+                path: null,
+                width: 750,
+                height: 750,
+                size: 96.89,
+                url: '/uploads/medium_IMG_7168_48fd09f204.JPG',
+              },
+            },
+            hash: 'IMG_7168_48fd09f204',
+            ext: '.JPG',
+            mime: 'image/jpeg',
+            size: 492.1,
+            url: '/uploads/IMG_7168_48fd09f204.JPG',
+            previewUrl: null,
+            provider: 'local',
+            provider_metadata: null,
+            createdAt: '2024-01-04T12:08:29.141Z',
+            updatedAt: '2024-01-04T12:08:29.141Z',
+          },
+        },
+      },
+    },
+  },
+  {
+    id: 2,
+    attributes: {
+      name: 'Олександр ',
+      surname: 'Жук ',
+      fathername: 'Володимирович',
+      role: 'Перший заступник Голови',
+      createdAt: '2023-12-31T00:49:03.388Z',
+      updatedAt: '2024-01-04T12:13:26.419Z',
+      publishedAt: '2023-12-31T00:49:04.123Z',
+      description: [
+        {
+          type: 'list',
+          format: 'unordered',
+          children: [
+            {
+              type: 'list-item',
+              children: [
+                {
+                  type: 'text',
+                  text: 'Народився 2 жовтня 2002 року',
+                },
+              ],
+            },
+            {
+              type: 'list-item',
+              children: [
+                {
+                  type: 'text',
+                  text: 'Бакалавр логістики, здобував освіту в Польші. Після того, як повернувся в Україну вступив в Одеську юридичну академію на юридичний факультет.',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          type: 'paragraph',
+          children: [
+            {
+              type: 'text',
+              text: 'Коментар щодо створення громадської організації:',
+            },
+          ],
+        },
+        {
+          type: 'quote',
+          children: [
+            {
+              type: 'text',
+              text: 'Ця ГО створена для комунікації з людьми та вирішення нагальних питань, які турбують громаду! Ми хочемо показати людям, що таке дійсно амбітна та дієва команда, розповідати щось більше не буду , краще все побачите самі!',
+            },
+          ],
+        },
+      ],
+      photo: {
+        data: {
+          id: 6,
+          attributes: {
+            name: 'IMG_7169.JPG',
+            alternativeText: null,
+            caption: null,
+            width: 1094,
+            height: 1280,
+            formats: {
+              thumbnail: {
+                name: 'thumbnail_IMG_7169.JPG',
+                hash: 'thumbnail_IMG_7169_d07e466751',
+                ext: '.JPG',
+                mime: 'image/jpeg',
+                path: null,
+                width: 133,
+                height: 156,
+                size: 7.34,
+                url: '/uploads/thumbnail_IMG_7169_d07e466751.JPG',
+              },
+              small: {
+                name: 'small_IMG_7169.JPG',
+                hash: 'small_IMG_7169_d07e466751',
+                ext: '.JPG',
+                mime: 'image/jpeg',
+                path: null,
+                width: 427,
+                height: 500,
+                size: 65.29,
+                url: '/uploads/small_IMG_7169_d07e466751.JPG',
+              },
+              large: {
+                name: 'large_IMG_7169.JPG',
+                hash: 'large_IMG_7169_d07e466751',
+                ext: '.JPG',
+                mime: 'image/jpeg',
+                path: null,
+                width: 855,
+                height: 1000,
+                size: 217.57,
+                url: '/uploads/large_IMG_7169_d07e466751.JPG',
+              },
+              medium: {
+                name: 'medium_IMG_7169.JPG',
+                hash: 'medium_IMG_7169_d07e466751',
+                ext: '.JPG',
+                mime: 'image/jpeg',
+                path: null,
+                width: 641,
+                height: 750,
+                size: 135.6,
+                url: '/uploads/medium_IMG_7169_d07e466751.JPG',
+              },
+            },
+            hash: 'IMG_7169_d07e466751',
+            ext: '.JPG',
+            mime: 'image/jpeg',
+            size: 301.91,
+            url: '/uploads/IMG_7169_d07e466751.JPG',
+            previewUrl: null,
+            provider: 'local',
+            provider_metadata: null,
+            createdAt: '2024-01-04T12:08:28.980Z',
+            updatedAt: '2024-01-04T12:08:28.980Z',
+          },
+        },
+      },
+    },
+  },
+  {
+    id: 3,
+    attributes: {
+      name: 'Анастасія ',
+      surname: 'Заєць ',
+      fathername: 'Валеріївна',
+      role: 'Секретар',
+      createdAt: '2024-01-04T12:15:56.369Z',
+      updatedAt: '2024-01-04T12:15:57.061Z',
+      publishedAt: '2024-01-04T12:15:57.059Z',
+      description: [
+        {
+          type: 'list',
+          format: 'unordered',
+          children: [
+            {
+              type: 'list-item',
+              children: [
+                {
+                  type: 'text',
+                  text: 'Народилася 5 жовтня 1996 року',
+                },
+              ],
+            },
+            {
+              type: 'list-item',
+              children: [
+                {
+                  type: 'text',
+                  text: 'У 2018 році закінчила Національний університет фізичного виховання і спорту України, де здобула ступінь магістра «Фізичної культури і спорту»',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          type: 'paragraph',
+          children: [
+            {
+              type: 'text',
+              text: 'Під час навчання приймала участь у всеукраїнських та міжнародних змаганнях бально-спортивного танцю. На даний момент працює тренером з виду спорту у селі Щасливе.',
+            },
+          ],
+        },
+        {
+          type: 'paragraph',
+          children: [
+            {
+              type: 'text',
+              text: 'Коментар щодо створення громадської організації:',
+            },
+          ],
+        },
+        {
+          type: 'quote',
+          children: [
+            {
+              type: 'text',
+              text: 'Молодь — це значна частина суспільства, яка може і вміє робити вчинки на користь прагнень громади. Здобути прозорість, дбати про культуру, стимулювати розвиток громади, сприяти вирішенню проблем та піклуватися про майбутнє!\nУ нас є спільна мета, тож зараз ми будемо разом працювати над її успіхом',
+            },
+          ],
+        },
+      ],
+      photo: {
+        data: {
+          id: 7,
+          attributes: {
+            name: 'IMG_0990.JPG',
+            alternativeText: null,
+            caption: null,
+            width: 1687,
+            height: 2363,
+            formats: {
+              thumbnail: {
+                name: 'thumbnail_IMG_0990.JPG',
+                hash: 'thumbnail_IMG_0990_0293f452f8',
+                ext: '.JPG',
+                mime: 'image/jpeg',
+                path: null,
+                width: 111,
+                height: 156,
+                size: 2.91,
+                url: '/uploads/thumbnail_IMG_0990_0293f452f8.JPG',
+              },
+              large: {
+                name: 'large_IMG_0990.JPG',
+                hash: 'large_IMG_0990_0293f452f8',
+                ext: '.JPG',
+                mime: 'image/jpeg',
+                path: null,
+                width: 714,
+                height: 1000,
+                size: 53.7,
+                url: '/uploads/large_IMG_0990_0293f452f8.JPG',
+              },
+              medium: {
+                name: 'medium_IMG_0990.JPG',
+                hash: 'medium_IMG_0990_0293f452f8',
+                ext: '.JPG',
+                mime: 'image/jpeg',
+                path: null,
+                width: 535,
+                height: 750,
+                size: 30.93,
+                url: '/uploads/medium_IMG_0990_0293f452f8.JPG',
+              },
+              small: {
+                name: 'small_IMG_0990.JPG',
+                hash: 'small_IMG_0990_0293f452f8',
+                ext: '.JPG',
+                mime: 'image/jpeg',
+                path: null,
+                width: 357,
+                height: 500,
+                size: 15,
+                url: '/uploads/small_IMG_0990_0293f452f8.JPG',
+              },
+            },
+            hash: 'IMG_0990_0293f452f8',
+            ext: '.JPG',
+            mime: 'image/jpeg',
+            size: 269.92,
+            url: '/uploads/IMG_0990_0293f452f8.JPG',
+            previewUrl: null,
+            provider: 'local',
+            provider_metadata: null,
+            createdAt: '2024-01-04T12:08:29.115Z',
+            updatedAt: '2024-01-04T12:08:29.115Z',
+          },
+        },
+      },
+    },
+  },
+];
--- src/pages/Main/Team/Team.jsx
+++ src/pages/Main/Team/Team.jsx
@@ -1,101 +1,16 @@
-import photo1 from 'images/photo1.png';
-import photo2 from 'images/photo2.png';
+import { team } from 'constants/team';
 import 'pages/Main/Team/Team.css';
 
 export default function Team() {
-  const people = [
-    {
-      id: 1,
-      name: 'Марія ',
-      surname: 'Шевченко ',
-      fathername: 'Юріївна',
-      role: 'Голова відділення',
-      photo: photo1,
-    },
-    {
-      id: 2,
-      name: 'Анатолій ',
-      surname: 'Овраменко ',
-      fathername: 'Ігорович',
-      role: 'Юрист',
-      photo: photo2,
-    },
-    {
-      id: 1,
-      name: 'Марія ',
-      surname: 'Шевченко ',
-      fathername: 'Юріївна',
-      role: 'Голова відділення',
-      photo: photo1,
-    },
-    {
-      id: 2,
-      name: 'Анатолій ',
-      surname: 'Овраменко ',
-      fathername: 'Ігорович',
-      role: 'Юрист',
-      photo: photo2,
-    },
-    {
-      id: 1,
-      name: 'Марія ',
-      surname: 'Шевченко ',
-      fathername: 'Юріївна',
-      role: 'Голова відділення',
-      photo: photo1,
-    },
-    {
-      id: 2,
-      name: 'Анатолій ',
-      surname: 'Овраменко ',
-      fathername: 'Ігорович',
-      role: 'Юрист',
-      photo: photo2,
-    },
-    {
-      id: 1,
-      name: 'Марія ',
-      surname: 'Шевченко ',
-      fathername: 'Юріївна',
-      role: 'Голова відділення',
-      photo: photo1,
-    },
-    {
-      id: 2,
-      name: 'Анатолій ',
-      surname: 'Овраменко ',
-      fathername: 'Ігорович',
-      role: 'Юрист',
-      photo: photo2,
-    },
-    {
-      id: 1,
-      name: 'Марія ',
-      surname: 'Шевченко ',
-      fathername: 'Юріївна',
-      role: 'Голова відділення',
-      photo: photo1,
-    },
-    {
-      id: 2,
-      name: 'Анатолій ',
-      surname: 'Овраменко ',
-      fathername: 'Ігорович',
-      role: 'Юрист',
-      photo: photo2,
-    },
-  ];
-
   return (
     <section className='team' id='team'>
       <h2 className='title underline'>Команда</h2>
       <ul className='team-list'>
-        {people.map(({ id, name, surname, fathername, role, photo }) => (
+        {team.map(({ id, attributes: { name, surname, fathername, role, photo } }) => (
           <li className='person-card' key={id}>
             <p className='text-xs role'>{role}</p>
             <p className='text name'>{`${surname} ${name} ${fathername}`}</p>
-            <img src={photo} alt='person' />
-            <button className='contacts-btn text-s'>Контакти</button>
+            <img src={`http://localhost:1337${photo.data.attributes.url}`} alt='person' />
           </li>
         ))}
       </ul>
